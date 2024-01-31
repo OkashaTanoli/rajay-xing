8d51fb674e3425ec7b94971624789280e7b28155
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2024-02-01 04:07:31 +0500

    updated populate in model

--- src/models/token.model.ts
+++ src/models/token.model.ts
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import User from '@/models/user.model'
+import Entry from '@/models/entry.model'
 
 const tokenSchema = new mongoose.Schema({
     type: String,
@@ -6,8 +8,8 @@ const tokenSchema = new mongoose.Schema({
     cnic: String,
     driverName: String,
     image: String,
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    entry: { type: mongoose.Schema.Types.ObjectId, ref: 'Entry', required: true }
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: User, required: true },
+    entry: { type: mongoose.Schema.Types.ObjectId, ref: Entry, required: true }
 }, { timestamps: true });
 
 export default mongoose.models.Token || mongoose.model('Token', tokenSchema);
